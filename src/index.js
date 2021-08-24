debead92e8d6016c99dde555856d87137608426b
Erick Contreras <eri89--@hotmail.com>
2021-08-24 17:26:24 -0500

    HashRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 // Component file
 import TodoContainer from './functionBased/components/TodoContainer';
 // Stylesheet
