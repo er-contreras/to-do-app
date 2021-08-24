f3d738e83d062812c9950435528b10b1ecd342e9
Erick Contreras <eri89--@hotmail.com>
2021-08-24 12:25:51 -0500

    Create functionBased and classBased folders

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 // Component file
-import TodoContainer from './components/TodoContainer';
+import TodoContainer from './functionBased/components/TodoContainer';
 // Stylesheet
-import './App.css';
+import './functionBased/App.css';
 
 ReactDom.render(
   <React.StrictMode>
